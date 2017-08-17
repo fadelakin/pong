9bc2c966608f4d7ad69197e88bc0fc042a3fcc46
Fisher Adelakin <temidayoadelakin@gmail.com>
2017-08-17 15:26:10 -0700

    draw background

--- pong.js
+++ pong.js
@@ -1,8 +1,18 @@
+function Background() {
+
+}
+
+Background.prototype.draw = function(context) {
+    context.fillStyle = "#000"
+    context.fillRect(0, 0, game.width, game.height)
+}
+
+
 var canvas = $("canvas")[0]
 var game = new Game(canvas)
 
 game.entities = [
-
+    new Background()
 ]
 
 game.start()
