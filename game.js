3177895fbd078efce73976f6cbe8cd118de0cf5f
Fisher Adelakin <temidayoadelakin@gmail.com>
2017-08-17 15:20:49 -0700

    actual game loop // remove semicolons

--- game.js
+++ game.js
@@ -1,29 +1,29 @@
 // the game engine
 
 function Game(canvas) {
-    var self = this;
+    var self = this
 
-    this.context = canvas.getContext("2d");
-    this.width = canvas.width;
-    this.height = canvas.height;
+    this.context = canvas.getContext("2d")
+    this.width = canvas.width
+    this.height = canvas.height
 
     // keep track of key states
     // Eg.:
     //   game.keyPressed.up === true // while UP key is pressed
     //   game.keyPressed.up === false // while UP key is released
-    this.keyPressed = {};
+    this.keyPressed = {}
 
     $(canvas).on('keydown keyup', function(e) {
         // convert key code to key name
-        var keyName = Game.keys[e.which];
+        var keyName = Game.keys[e.which]
 
         if (keyName) {
             // eg.: `self.keyPressed.up = true` on keydown
             // will be set to `false` on keyup
-            self.keyPressed[keyName] = e.type === 'keydown';
-            e.preventDefault();
+            self.keyPressed[keyName] = e.type === 'keydown'
+            e.preventDefault()
         }
-    });
+    })
 }
 
 // some key codes to key name mapping
@@ -33,4 +33,15 @@ Game.keys = {
     38: 'up',
     39: 'right',
     40: 'down'
-};
+}
+
+Game.prototype.start = function() {
+    var self = this
+    var fps = 60
+    var interval = 1000 / fps // ms per frame
+
+    setInterval(function() {
+        self.update()
+        self.draw()
+    }, interval)
+}
