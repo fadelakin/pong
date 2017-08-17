d88f96a4bb69ab658c4b5badbee125dc014aa3a8
Fisher Adelakin <temidayoadelakin@gmail.com>
2017-08-17 15:58:05 -0700

    chrome is weird

--- paddles.js
+++ paddles.js
@@ -1,5 +1,11 @@
 function Paddle() {
     Entity.call(this)
+
+    this.width = 20
+    this.height = 100
+
+    this.y = game.height / 2 - this.height / 2
+    this.x = 20
 }
 
 Paddle.prototype = Object.create(Entity.prototype)
